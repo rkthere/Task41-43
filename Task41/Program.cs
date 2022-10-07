65cd1a280b6e42e848f4baf553a90284a85f5c8a
Vlad <rkt9021@mail.ru>
2022-10-07 16:06:27 +0300

    Добавили файлы

---
+++ Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//-1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите размер массива:");
+int len = int.Parse(Console.ReadLine()!);
+int[] arr = new int[len];
+Console.WriteLine("Введите случайны числа через запятую, в соответствие указанному размеру массива:");
+string[] arrayNumber = Console.ReadLine()!.Split(',');
+
+int CheckNumber(int[] arr)
+{
+    int NumbersGreaterThenZero = 0;
+    for (int i = 0; i < arrayNumber.Length; i++)
+    {
+        arr[i] = int.Parse(arrayNumber[i]);
+        if(arr[i] > 0)
+        {
+            NumbersGreaterThenZero++;
+        }  
+    }
+     return NumbersGreaterThenZero;
+}
+int result = CheckNumber(arr);
+Console.WriteLine($"Количество элементов больше 0 -> {result}");
+
---
+++ Task43/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых,
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.WriteLine("Введите точку b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите точку k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите точку b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите точку k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+
+double intersection(double b1, double k1, double b2, double k2)
+{
+    double y = 0;
+    double x = (-b2 + b1)/(-k1 + k2);
+    if(b1 > 0 && k1 > 0 && b2 > 0 && k2 > 0)
+    {
+        y = k1 * x + b1;
+        y = k2 * x + b2;
+    }
+    return x;
+}
+double result = Convert.ToDouble(intersection(b1,k1,b2,k2));
+Console.WriteLine($"Точка пересечения двух прямых => {result}");
